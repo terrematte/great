680472d1ac9b1443b57f74a296c9e9aa7d75bd49
dadn2002 <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>
2025-02-13 19:12:04 -0300

    update branch commit

---
+++ optimization_test.py
@@ -0,0 +1,31 @@
+import subprocess
+import json
+import matplotlib.pyplot as plt
+
+def call_shell_script():
+    result = subprocess.run(['bash', 'example_requests/default_request.sh'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    try:
+        data = json.loads(output)
+        return data.get('time', None)
+    except json.JSONDecodeError:
+        print("Error decoding JSON")
+        return None
+
+times = []
+num_calls = 100
+
+for _ in range(num_calls):
+    time = call_shell_script()
+    if time is not None:
+        times.append(time)
+    else:
+        times.append(0)
+
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, num_calls + 1), times, marker='o', linestyle='-', color='b')
+plt.title('Time Taken by default_request.sh')
+plt.xlabel('Call Number')
+plt.ylabel('Time (seconds)')
+plt.grid(True)
+plt.show()
